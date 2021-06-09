a15c8c86b208090e91267d5c19c4b6ca536937fb
israaothman <israaayoub@hotmail.com>
2021-06-09 16:54:19 +0300

    add heart img

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import heart from '../assets/hhhh.png';
 
 export class HornedBeasts extends Component {
-    static propTypes = {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfVotes: 0
+        }
+    }
 
+    addVote =()=>{
+      let newVotes=this.state.numOfVotes;
+      this.setState({
+        numOfVotes:newVotes++
+      })
     }
 
     render() {
         return (
-            <div>
+            <div onClick={this.addVote}>
                 <h2>{this.props.title}</h2>
                 <img src={this.props.imageUrl} alt={this.props.title} title={this.props.title}/>
                 <p>{this.props.description}</p>
+                <img src={heart} width="40px" height="40px" />
+                <span>{this.state.numOfVotes}</span>
             </div>
         )
     }
